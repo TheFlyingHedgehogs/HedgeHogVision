856ffc702eda35541c214562f0370aca0071b077
Ozy King <ozypfl@gmail.com>
2023-01-10 16:20:00 -0800

    Re-formatted code

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 from numpy.typing import ArrayLike
 import pupil_apriltags
 import cv2
+import math
 from pprint import pprint
 
 @dataclass
@@ -27,32 +28,43 @@ def perfect_camera(focal_length_mm, horizontal_sensor_size_mm, resolution) -> Pi
     return PinholeCalibration(mtx, dist)
 
 class FoundTag:
-    id: int
-    translation: ArrayLike
-    rotation: ArrayLike
+    def __init__(self, parent_tag: int, translation: ArrayLike, rotation: ArrayLike):
+        parent_tag: KnownTag = parent_tag
+        """The ID of the apriltag"""
+        translation: ArrayLike = translation
+        """Translation of the camera from the apriltag"""
+        rotation: ArrayLike = rotation
+        """Rotation matrix of the apriltag from the matrix"""
 
 
 @dataclass
 class KnownTag:
-    x: float
-    y: float
-    z: float
-    rotation: float
+    """Contains the position and rotation of the tag on the field"""
+    def __init__(self, x_inches: float, y_inches: float, z_inches: float, rotation_degrees: float):
+        self.x: float = x_inches * 0.0254
+        """X position of the tag relative to a corner of the field in meters"""
+        self.y: float = y_inches * 0.0254
+        """Y position of the tag relative to a corner of the field in meters"""
+        self.z: float = z_inches * 0.0254
+        """Z position of the tag relative to a corner of the field in meters"""
+        self.rotation: float = math.radians(rotation_degrees)
+        """Rotation of the tag relative to the center of the field in radians."""
 
 
-def t(x_in: float, y_in: float, z_in: float, rotation: float) -> KnownTag:
+"""def t(x_in: float, y_in: float, z_in: float, rotation: float) -> KnownTag:
     return KnownTag(x_in * 0.0254, y_in * 0.0254, z_in * 0.0254, rotation)
-
-field = {
-    1: t( 42.19, 610.77, 18.22, 180),
-    2: t(108.19, 610.77, 18.22, 180),
-    3: t(147.19, 610.77, 18.22, 180),
-    4: t(265.74, 636.96, 27.38, 180),
-    5: t(265.74,  14.25, 27.38, 0),
-    6: t(147.19,  40.45, 18.22, 0),
-    7: t(108.19,  40.45, 18.22, 0),
-    8: t( 42.19,  40.45, 18.22, 0)
-}
+"""
+field = (
+    None,                                   #0
+    KnownTag(42.19, 610.77, 18.22, 180),    #1
+    KnownTag(108.19, 610.77, 18.22, 180),   #2
+    KnownTag(147.19, 610.77, 18.22, 180),   #3
+    KnownTag(265.74, 636.96, 27.38, 180),   #4
+    KnownTag(265.74,  14.25, 27.38, 0),     #5
+    KnownTag(147.19,  40.45, 18.22, 0),     #6
+    KnownTag(108.19,  40.45, 18.22, 0),     #7
+    KnownTag(42.19,  40.45, 18.22, 0)       #8
+)
 
 
 class Detector:
@@ -69,6 +81,7 @@ class Detector:
         ], dtype=np.float64)
     
     def find_tags(self, image: ArrayLike) -> list[FoundTag()]:
+        """Returns a list of all found tags in the frame"""
         gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
         found = self.detector.detect(gray)
 
@@ -79,17 +92,13 @@ class Detector:
                 continue
             image_points = np.array(item.corners)
 
-            ret, rvec, tvec = cv2.solvePnP(self.object_points, image_points, self.calibration.mtx, self.calibration.dist)
-            if ret:
-                r_matrix = cv2.Rodrigues(rvec)[0]
+            success, rotation_vector, translation_vector = cv2.solvePnP(self.object_points, image_points, self.calibration.mtx, self.calibration.dist)
+            if success:
+                rotation_matrix = cv2.Rodrigues(rotation_vector)[0]
 
-                tag_coords = np.matmul(r_matrix, tvec)
+                tag_coords = np.matmul(rotation_matrix, translation_vector)
 
                 known_pos = field[item.tag_id]
-                # print(item.tag_id)
-                # print(field)
-                # print(field[item.tag_id])
-
                 """
                   x
                  ___
@@ -99,16 +108,15 @@ class Detector:
                 
                 vertical is z
                 """
-
-
                 if known_pos == None:
                     continue
+                detected.append(FoundTag(known_pos, translation_vector, rotation_matrix))
                 print(tag_coords)
                 # print(known_pos)
-                print(tvec)
+                print(translation_vector)
                 world_coords = (known_pos.x + tag_coords[0][0], known_pos.y + tag_coords[2][0], known_pos.z + tag_coords[1][0])
                 # print(world_coords)
-
+        return detected
 
 img = cv2.imread("/tmp/0251.png")
 # img = cv2.imread("/home/foo/synced/2023-field/angles/moved.png")
