14829b01e2d7cf1b2014f2b24b7eb1bab93efc5a
Ozy King <ozypfl@gmail.com>
2023-01-11 16:55:31 -0800

    Stuff

--- math_stuff.py
+++ math_stuff.py
@@ -2,25 +2,6 @@ from dataclasses import dataclass
 from quaternions.quaternions import Quaternion
 
 
-class Translation3d:
-    pass
-
-
-@dataclass
-class Translation3d:
-    x: float
-    y: float
-    z: float
-
-    def unary_minus(self) -> Translation3d:
-        return Translation3d(-self.x, -self.y, -self.z)
-
-    def rotate_by(self, r):
-        p = Quaternion(0.0, self.x, self.y, self.z)
-        qprime = r.q * p * r.q.conjugate()
-        return Translation3d(qprime.real, qprime.i, qprime.j)
-
-
 @dataclass
 class Rotation3d:
     q: Quaternion
@@ -28,13 +9,29 @@ class Rotation3d:
     def unary_minus(self):
         return Rotation3d(self.q.conjugate())
 
+@dataclass
+class Translation3d:
+    x: float
+    y: float
+    z: float
+
+    def unary_minus(self):
+        return Translation3d(-self.x, -self.y, -self.z)
+
+    def rotate_by(self, other: Rotation3d):
+        p = Quaternion(0.0, self.x, self.y, self.z)
+        qprime = other.q * p * other.q.conjugate()
+        return Translation3d(qprime.real, qprime.i, qprime.j)
+
+
+
 
 @dataclass
 class Transform3d:
     translation: Translation3d
     rotation: Rotation3d
 
-    def inverse(self) -> Transform3d:
+    def inverse(self):
         return Transform3d(
             self.translation.rotate_by(self.rotation.unary_minus()),
             self.rotation.unary_minus()
